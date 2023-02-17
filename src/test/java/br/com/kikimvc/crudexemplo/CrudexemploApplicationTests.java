ffa4aae5279afe1007c2ae3282b1ed4a42c3da8d
luish <luishenrique.farina@gmail.com>
2023-02-17 10:45:38 -0300

    setup inicial

---
+++ src/test/java/br/com/kikimvc/crudexemplo/CrudexemploApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kikimvc.crudexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudexemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
