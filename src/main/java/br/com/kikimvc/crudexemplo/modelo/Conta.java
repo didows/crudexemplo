fa570153c5034c2d9137a838db232d1644cfec74
luish <luishenrique.farina@gmail.com>
2023-02-17 12:23:10 -0300

    Conta

--- src/main/java/br/com/kikimvc/crudexemplo/modelo/Conta.java
+++ src/main/java/br/com/kikimvc/crudexemplo/modelo/Conta.java
@@ -1,11 +1,49 @@
 package br.com.kikimvc.crudexemplo.modelo;
 
+import java.util.Date;
+
 public class Conta {
 
         private Long id;
         private String descricao;
         private EnumTipoConta tipo;
         private Pessoa pessoa;
+
+        private Date dataPagamento;
+
+        private Date dataVencimento;
+
+        private Double valor;
+
+        private Boolean pago;
+
+        public Boolean getPago() {
+            return pago;
+        }
+
+        public void setPago(Boolean pago) {
+            this.pago = pago;
+        }
+
+        public Double getValor() {
+            return valor;
+        }
+
+        public void setValor(Double valor) {
+            this.valor = valor;
+        }
+
+        public void setDataVencimento(Date dataVencimento) {
+            this.dataVencimento = dataVencimento;
+        }
+
+        public Date getDataVencimento() {
+            return dataVencimento;
+        }
+
+        public Date getDataPagamento() {
+            return dataPagamento;
+        }
 
         public Long getId() {
             return id;
